dadded4f0ce1292b5a3050c2580d9b131311072a
Anandmishra0807 <146430635+Anandmishra0807@users.noreply.github.com>
2024-06-26 00:36:15 +0530

    home page created

---
+++ Student_management/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="header.css">
+</head>
+<body>
+<div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="home.php" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="gallery.php" class="nav-link">Gallery</a></li>
+        <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
+        <li class="nav-item"><a href="registration.php" class="nav-link">Registration</a></li>
+      </ul>
+    </header>
+  </div>
+</body>
+</html>
--- profile.php
+++ profile.php
@@ -1,3 +1,5 @@
 <?php
     echo "hello ".$_GET['q'];
 ?>
+
+<a href="logout.php?username=<?php echo .$_GET['q'] ?>&message=done now">Logout</a>
